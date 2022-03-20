f42bdb9810ec4fa08d808f829f47b1c4ef3b2044
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-03-20 10:58:25 +0530

    changes-11873

---
+++ src/test/java/project02/CountriesTest.java
@@ -0,0 +1,14 @@
+package project02;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+
+public class CountriesTest extends BaseTest {
+
+    // https://restcountries.com/v2/alpha/in
+
+    @Test
+    public void shouldBeAbleToGetCountryDetails(){
+
+    }
+}
